d654d7fe27e6239c5b04be0446420ac0670fb73c
Rajan lahoti <rajan.lahoti@espire.com>
2021-09-14 16:46:45 +0530

    first commit

---
+++ CollectionAssignment/CountElementsInPriorityQueue.java
@@ -0,0 +1,21 @@
+package CollectionAssignment;
+import java.util.PriorityQueue;
+
+public class CountElementsInPriorityQueue  {
+	public static void main(String ag[]) {
+		PriorityQueue<String> queue=new PriorityQueue<String>(); 
+			queue.add("Blue");
+			queue.add("black");
+			queue.add("green");
+			queue.add("maroon");
+			queue.add("red");
+			System.out.println("Queue: "+queue);
+			
+			queue.size();
+			System.out.println("Queue: "+queue.size());
+	}
+	
+	
+	
+}
+			
